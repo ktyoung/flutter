716fb0d1930d372daa95c629dc0e797a8b08fe00
ktyoung <ktyoung0507@gmail.com>
2022-12-05 17:09:08 +0900

    Widgets

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -17,12 +17,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // 여기에 코드 작성
 
+      // ===== 1. 글자 위젯 Text('텍스트') ===== //
       // home: Text('안녕!')
 
+
+      // ===== 2. 아이콘 위젯 Icon(Icons.아이콘이름) ===== //
       // home: Icon(Icons.star)
 
+      // ===== 3. 이미지 위젯 Image.asset('경로') ===== //
+      // → 프로젝트에 이미지 보관용 assets 폴더 생성 후 이미지 붙여넣기
+      // → pubspec.yaml에 assets 폴더 사용하겠다고 명시하기
       // home: Image.asset('example.png')
 
+      // ===== 4. 네모 박스 위젯 Container( 스타일명: 값 ) ===== //
       // Flutter의 사이즈 단위는 LP → 50LP == 1.2cm
       // 화면에 꽉차게 나오는 이유? 박스를 그리는 좌표(기준점)를 모르기 때문
       // home: Container( width: 50, height: 50, color: Colors.cyan)
@@ -36,11 +43,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// 반드시 알아야 하는 위젯 4가지
-// 1. 글자 위젯 Text('텍스트')
-// 2. 이미지 위젯 Image.asset('경로')
-// → 프로젝트에 이미지 보관용 assets 폴더 생성 후 이미지 붙여넣기
-// → pubspec.yaml에 assets 폴더 사용하겠다고 명시하기
-// 3. 아이콘 위젯 Icon(Icons.아이콘이름)
-// 4. 네모 박스 위젯 Container( 스타일명: 값 )
