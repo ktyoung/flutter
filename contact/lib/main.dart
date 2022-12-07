fa1d0991333f68210afa27818b7a10f566378847
ktyoung <ktyoung0507@gmail.com>
2022-12-07 19:44:11 +0900

    컨테이너 디자인

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -10,26 +10,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("앱 이름"),
-        ),
-        body: Container(
-          child: Text("내용"),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          // width, height, child만 필요한 박스는 SizedBox() → Container보다 가벼움
-          child: SizedBox(
-            height: 70,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon( Icons.phone ),
-                Icon( Icons.message ),
-                Icon( Icons.contact_page ),
-              ],
-            ),
-          ),
-        )
+        appBar: AppBar( title: Text("앱 이름") ),
+       body: Container(
+         width : 150, height: 150, color: Colors.blue,
+         // 개별 방향에 바깥 여백(margin)
+         margin: EdgeInsets.fromLTRB(10, 20, 30, 40),
+         // 모든 방향에 안쪽 여백(padding)
+         padding: EdgeInsets.all(20),
+        
+         child: Text("텍스트"),
+       ),
       ),
     );
   }
