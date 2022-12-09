be047d6acb1d8759593eb7a56fcd0c9b43d0ee6f
ktyoung <ktyoung0507@gmail.com>
2022-12-09 15:14:46 +0900

    AppBar

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -11,14 +11,58 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("앱 이름"),
-          // leading: AppBar 왼쪽에 넣을 아이콘
-          leading: Icon(Icons.star),
-          // actions: [AppBar 우측에 넣을 아이콘]
-          actions: [ Icon(Icons.star), Icon(Icons.star), Icon(Icons.star), ],
+          title: Text("금호동 3가"),
+          actions: [
+            Icon(Icons.search, size: 40),
+            Icon(Icons.menu, size: 40),
+            Icon(Icons.alarm, size: 40)
+          ],
         ),
-        body: SizedBox(),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          child: Row(
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                child: Row(
+                  children: [
+                    Image.asset("assets/example.png"),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("강아지",
+                          style: TextStyle(fontWeight: FontWeight.w400, fontSize: 30)
+                        ),
+                        Text("성동구 행당동·끌올 10분 전",
+                          style: TextStyle(color: Color(0xffaaaaaa)),
+                        ),
+                        Text("990,000원",
+                          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(Icons.favorite_border, color: Color(0xff888888),),
+                            Text("4", style: TextStyle(color: Color(0xff888888),),
+                            ),
+                          ],
+                        )
+                      ],
+                    ) 
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      );
+      ),
+    );
   }
 }
