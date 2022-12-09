bfe82a0357040855e7f95f7a2881479c0b9ac420
ktyoung <ktyoung0507@gmail.com>
2022-12-09 14:18:00 +0900

    AppBar

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -12,11 +12,30 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar( title: Text("앱 이름") ),
         body: SizedBox(
-          // Icon() 위젯 디자인
-          // Icon() 위젯은 color, size만 디자인 가능함
-          child: Icon(Icons.star, color: Colors.red, size: 60,)
+          // 버튼 디자인
+          // 버튼에는 필히 child:, onPressed: 를 작성해야 한다
+
+          // 1. TextButton()
+          // child: TextButton(
+          //   child: Text("버튼 텍스트입니다."),
+          //   onPressed: () {},
+          // )
+
+          // 2. ElevatedButon()
+          // 배경색이 채워진 버튼
+          // child: ElevatedButton(
+          //   child: Text("버튼 텍스트입니다."),
+          //   onPressed: () {},
+          // )
+
+          // 3. IconButton()
+          child: IconButton(
+            // IconButton()에는 icon: 파라미터 사용
+            icon: Icon(Icons.star),
+            onPressed: () {},
           ),
-        )
+        ),
+      ),
     );
   }
 }
