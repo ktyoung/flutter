147f1d935aeab48c6dd92951cc9bc85394ba2450
ktyoung <ktyoung0507@gmail.com>
2022-12-09 14:20:49 +0900

    AppBar

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -10,32 +10,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar( title: Text("앱 이름") ),
-        body: SizedBox(
-          // 버튼 디자인
-          // 버튼에는 필히 child:, onPressed: 를 작성해야 한다
-
-          // 1. TextButton()
-          // child: TextButton(
-          //   child: Text("버튼 텍스트입니다."),
-          //   onPressed: () {},
-          // )
-
-          // 2. ElevatedButon()
-          // 배경색이 채워진 버튼
-          // child: ElevatedButton(
-          //   child: Text("버튼 텍스트입니다."),
-          //   onPressed: () {},
-          // )
-
-          // 3. IconButton()
-          child: IconButton(
-            // IconButton()에는 icon: 파라미터 사용
-            icon: Icon(Icons.star),
-            onPressed: () {},
-          ),
+        appBar: AppBar(
+          title: Text("앱 이름"),
+          // leading: AppBar 왼쪽에 넣을 아이콘
+          leading: Icon(Icons.star),
+          // actions: [AppBar 우측에 넣을 아이콘]
+          actions: [ Icon(Icons.star), Icon(Icons.star), Icon(Icons.star), ],
         ),
-      ),
-    );
+        body: SizedBox(),
+        ),
+      );
   }
 }
